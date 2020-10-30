7c7468446beb23dd01c5f2441b50cacda25e8615
Federico Senmartin <fede.senmartin@gmail.com>
2020-10-30 16:56:14 -0300

    initial commit

---
+++ app/src/main/java/com/example/composeweather/HomeModule/view/WeatherHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.composeweather.HomeModule.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    
+}
